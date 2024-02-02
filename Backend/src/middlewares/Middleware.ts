fceaaa67946a87c6fb3a50dcff9a812315eca757
Palasha <palashasoni5@gmail.com>
2024-02-02 11:50:09 +0530

    modified useractions, added auth , modified middleware

--- Backend/src/middlewares/Middleware.ts
+++ Backend/src/middlewares/Middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import DataEncryptor from "../helpers/DataEncryptor";
+import AuthManager  from "../helpers/Auth"
 
 class Middleware {
     dataEncryptor: DataEncryptor;
@@ -20,6 +21,22 @@ class Middleware {
             res.status(400).send('Invalid encrypted data');
         }
     }
+
+    jwtMiddleware = async (req: any, res: any, next: any) => {
+        const token = req.headers.authorization;
+    
+        if (!token) {
+            return res.status(401).json({ Error: "Unauthorized - Missing token" });
+        }
+    
+        try {
+            const decoded = await AuthManager.verifyToken(token);
+            req.user = decoded;
+            next();
+        } catch (error) {
+            return res.status(401).json({ Error: "Unauthorized - Invalid token" });
+        }
+    };
 }
 
 export default Middleware;
--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -1,59 +1,64 @@
 import ChatActions from "./actions/ChatActions";
 import UserActions from "./actions/UserActions";
 
-const Routes = [
-  {
-    method: 'post',
-    route: '/user/signIn',
-    action: UserActions.signIn,
-  },
-  {
-    method: 'post',
-    route: '/user/signUp',
-    action: UserActions.signUp,
-  },
-  {
-    method: 'put',
-    route: '/user/:userId/updateProfile',
-    action: UserActions.updateUserProfile,
-    // middleware: jwtMiddleware,
-  },
-  {
-    method: 'delete',
-    route: '/user/:userId/deleteAccount',
-    action: UserActions.deleteAccount,
-    // middleware: jwtMiddleware,
-  },
-  {
-    method: 'get',
-    route: '/chat/:userId/getChatsList',
-    action: ChatActions.getChatsList,
-    // middleware: jwtMiddleware,
-  },
-  {
-    method: 'post',
-    route: '/chat/:userId/start',
-    action: ChatActions.startChat,
-    // middleware: jwtMiddleware,
-  },
-  {
-    method: 'post',
-    route: '/chat/:chatId/sendQuery',
-    action: ChatActions.sendQuery,
-    // middleware: jwtMiddleware,
-  },
-  {
-    method: 'put',
-    route: '/chat/:chatId/endChat',
-    action: ChatActions.endChat,
-    // middleware: jwtMiddleware,
-  },
-  {
-    method: 'delete',
-    route: '/chat/:chatId/deleteChat',
-    action: ChatActions.deleteChat,
-    // middleware: jwtMiddleware,
-  },
-];
+const Routes: Array<{
+  method: string;
+  route: string;
+  action: (...args: any[]) => void;
+  middleware?: any;
+}> = [
+    {
+      method: 'post',
+      route: '/user/signIn',
+      action: UserActions.signIn,
+    },
+    {
+      method: 'post',
+      route: '/user/signUp',
+      action: UserActions.signUp,
+    },
+    {
+      method: 'put',
+      route: '/user/:userId/updateProfile',
+      action: UserActions.updateUserProfile,
+      // middleware: jwtMiddleware,
+    },
+    {
+      method: 'delete',
+      route: '/user/:userId/deleteAccount',
+      action: UserActions.deleteAccount,
+      // middleware: jwtMiddleware,
+    },
+    {
+      method: 'get',
+      route: '/chat/:userId/getChatsList',
+      action: ChatActions.getChatsList,
+      // middleware: jwtMiddleware,
+    },
+    {
+      method: 'post',
+      route: '/chat/:userId/start',
+      action: ChatActions.startChat,
+      // middleware: jwtMiddleware,
+    },
+    {
+      method: 'post',
+      route: '/chat/:chatId/sendQuery',
+      action: ChatActions.sendQuery,
+      // middleware: jwtMiddleware,
+    },
+    {
+      method: 'put',
+      route: '/chat/:chatId/endChat',
+      action: ChatActions.endChat,
+      // middleware: jwtMiddleware,
+    },
+    {
+      method: 'delete',
+      route: '/chat/:chatId/deleteChat',
+      action: ChatActions.deleteChat,
+      // middleware: jwtMiddleware,
+    },
+  ];
 
 export default Routes;
